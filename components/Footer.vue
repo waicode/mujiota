3b2fcb9ca07edff0b1710cb7269cfc7c8858ebd4
waicode <waicode37@gmail.com>
2021-06-21 00:14:28 +0900

    feat: header footer

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>© 2021mujiota.com</p>
+    </div>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <img src="@/assets/image/shared/mujiota-logo.svg" />
+      </div>
+    </nav>
+  </header>
+</template>
