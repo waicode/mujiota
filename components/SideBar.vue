b8058736fdd86ab6c934d0a79563b0b510f4e98b
waicode <waicode37@gmail.com>
2021-06-23 23:03:32 +0900

    feat: sidebar

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -12,10 +12,38 @@
       <li><h3>aaa</h3></li>
     </ol>
     <h2>コンセプト</h2>
+    <div>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+    </div>
     <h2>カテゴリー</h2>
+    <div>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+    </div>
     <h2>アーカイブ</h2>
+    <div>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+    </div>
     <div class="ad-side-top">AD 250x250</div>
     <h2>リコメンド</h2>
+    <div>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+      <p>aaa</p>
+    </div>
   </div>
 </template>
 
