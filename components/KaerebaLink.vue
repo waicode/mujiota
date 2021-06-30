7efcfad5ef21694e24783b0ee85fb3a25b2732ff
waicode <waicode37@gmail.com>
2021-06-30 23:16:15 +0900

    feat: kaereba

---
+++ components/KaerebaLink.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="cstmreba">
+    <div class="kaerebalink-box">
+      <div class="kaerebalink-image">
+        <a
+          href="http://www.amazon.co.jp/exec/obidos/ASIN/B00L8IZ1E8/amayutazon-22/"
+          target="_blank"
+          rel="nofollow"
+          class="no-icon"
+          ><img
+            src="https://images-fe.ssl-images-amazon.com/images/I/51VK4d-mqRL._SL160_.jpg"
+            style="border: none"
+        /></a>
+      </div>
+      <div class="kaerebalink-info">
+        <div class="kaerebalink-name">
+          <a
+            href="http://www.amazon.co.jp/exec/obidos/ASIN/B00L8IZ1E8/amayutazon-22/"
+            target="_blank"
+            rel="nofollow"
+            >{{ title }}</a
+          >
+        </div>
+        <div class="kaerebalink-detail">{{ shopName }}</div>
+        <div class="kaerebalink-link1">
+          <div class="shoplinkamazon">
+            <a
+              href="http://www.amazon.co.jp/gp/search?keywords=%E3%82%B0%E3%83%A9%E3%83%B3%E3%83%95%E3%82%A7%E3%83%AB%E3%83%9E%E3%83%BC%E3%82%B8%E3%83%A5%20%E3%83%90%E3%82%A4%E3%82%AA%E3%83%BB%E3%82%B0%E3%83%A9%E3%82%B9%E3%83%95%E3%82%A7%E3%83%83%E3%83%89%E3%83%90%E3%82%BF%E3%83%BC%20%E7%84%A1%E5%A1%A9%20250g&amp;__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&amp;tag=amayutazon-22"
+              target="_blank"
+              rel="nofollow"
+              >Amazon</a
+            >
+          </div>
+          <div class="shoplinkrakuten">
+            <a
+              href="https://hb.afl.rakuten.co.jp/hgc/07687bed.c16bc13c.0d960c31.3eeb42bc/?pc=http%3A%2F%2Fsearch.rakuten.co.jp%2Fsearch%2Fmall%2F%25E3%2582%25B0%25E3%2583%25A9%25E3%2583%25B3%25E3%2583%2595%25E3%2582%25A7%25E3%2583%25AB%25E3%2583%259E%25E3%2583%25BC%25E3%2582%25B8%25E3%2583%25A5%2520%25E3%2583%2590%25E3%2582%25A4%25E3%2582%25AA%25E3%2583%25BB%25E3%2582%25B0%25E3%2583%25A9%25E3%2582%25B9%25E3%2583%2595%25E3%2582%25A7%25E3%2583%2583%25E3%2583%2589%25E3%2583%2590%25E3%2582%25BF%25E3%2583%25BC%2520%25E7%2584%25A1%25E5%25A1%25A9%2520250g%2F-%2Ff.1-p.1-s.1-sf.0-st.A-v.2%3Fx%3D0%26scid%3Daf_ich_link_urltxt%26m%3Dhttp%3A%2F%2Fm.rakuten.co.jp%2F"
+              target="_blank"
+              rel="nofollow"
+              >楽天市場</a
+            >
+          </div>
+        </div>
+      </div>
+      <div class="booklink-footer"></div>
+    </div>
+  </div>
+</template>
