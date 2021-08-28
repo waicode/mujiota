8235757f62a47c37ccf651bb2da2b3f820c57424
waicode <waicode37@gmail.com>
2021-08-28 22:33:58 +0900

    feat: RelatedPosts

--- components/Parts/RelatedPosts.vue
+++ components/Parts/RelatedPosts.vue
@@ -4,15 +4,23 @@
     <div class="related-posts">
       <ul class="related-list">
         <li v-for="article in articles" :key="article.id">
-          <a href="#">
-            <div class="eyecatch">
-              <AssetsImage
-                :path="`images/eyecatch/${article.id}/${article.slug}.${article.imageFormat}`"
-              />
+          <NuxtLink :to="`/${article.id}/${article.slug}`">
+            <div class="wrap">
+              <div class="eyecatch">
+                <AssetsImage
+                  :path="`images/eyecatch/${article.id}/${article.slug}.${article.imageFormat}`"
+                />
+              </div>
+              <div class="contents">
+                <h3 class="post-title">{{ article.title }}</h3>
+                <div class="meta-wrap">
+                  <div class="date">
+                    {{ article.updatedAt | dateFormatted }}
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="date">{{ article.updatedAt | dateFormatted }}</div>
-            <h3 class="related-post-title">{{ article.title }}</h3>
-          </a>
+          </NuxtLink>
         </li>
       </ul>
     </div>
@@ -52,6 +60,56 @@ export default {
   &::after {
     margin-left: 32px;
     transform: rotate(90deg);
+  }
+}
+.related-list {
+  .wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    .eyecatch {
+      display: block;
+      padding: 24px;
+      width: 40%;
+      height: auto;
+      img {
+        vertical-align: top;
+      }
+    }
+    .contents {
+      width: 56%;
+      .post-title {
+        font-size: 1.24rem;
+        font-weight: bold;
+        color: $text;
+        margin-bottom: 8px;
+      }
+      .description {
+        font-size: 0.86rem;
+        color: $text;
+        margin-bottom: 12px;
+      }
+      .meta-wrap {
+        display: flex;
+        height: 40px;
+        justify-content: space-between;
+        align-items: center;
+        color: $text;
+        .date {
+          display: flex;
+          font-size: 0.8rem;
+          padding-top: 2px;
+          .date-published {
+            margin-right: 12px;
+          }
+        }
+        .tags {
+          display: flex;
+          font-size: 0.8rem;
+        }
+      }
+    }
   }
 }
 </style>
