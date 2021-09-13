c6ee783ced8bf0e7a9d163a049cff1714cb99d11
waicode <waicode37@gmail.com>
2021-09-13 22:46:20 +0900

    feat: paganation resolve #1 resolve #2

--- pages/tag/_slug/index.vue
+++ pages/tag/_slug/index.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="container">
     <div class="post-list">
-      <div v-for="(article, index) in articles" :key="article.id">
+      <div v-for="(article, index) in displayPosts" :key="article.id">
         <Article :article="article" />
         <hr v-if="index < articles.length - 1" :key="`hr-${article.id}`" />
       </div>
+    </div>
+    <div class="post-pagination">
+      <b-pagination
+        v-model="currentPage"
+        :total="articles.length"
+        :range-before="1"
+        :range-after="1"
+        size="is-large"
+        :per-page="pageSize"
+        icon-prev="chevron-left"
+        icon-next="chevron-right"
+      >
+      </b-pagination>
     </div>
   </div>
 </template>
@@ -38,6 +51,21 @@ export default {
         statusCode: 404,
       })
     }
+  },
+  data() {
+    return {
+      currentPage: 1,
+      pageSize: 5,
+    }
+  },
+  computed: {
+    displayPosts() {
+      // TODO: 記事の高さを固定にしてチラツキをなくす
+      return this.articles.slice(
+        this.pageSize * (this.currentPage - 1),
+        this.pageSize * this.currentPage
+      )
+    },
   },
 }
 </script>
