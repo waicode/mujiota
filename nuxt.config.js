75cbc091336a930409cfad653dd6fbd910abc63b
waicode <waicode37@gmail.com>
2021-09-21 16:19:49 +0900

    fix: vercel setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   env: envSettings,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
