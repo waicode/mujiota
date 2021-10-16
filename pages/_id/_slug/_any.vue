b841d8aab04cbfaea3c05be7c1224993b373271d
waicode <waicode37@gmail.com>
2021-10-16 21:56:36 +0900

    feat: add post page redirect

---
+++ pages/_id/_slug/_any.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  },
+  async asyncData({ $content, params, redirect, error }) {
+    let articles = []
+    try {
+      articles = await $content('articles', params.id, { deep: true }).fetch()
+      if (articles.length === 1) {
+        const article = articles[0]
+        redirect(301, `/${article.id}/${article.slug}`)
+      } else {
+        error({
+          statusCode: 404,
+        })
+      }
+    } catch {
+      error({
+        statusCode: 404,
+      })
+    }
+  },
+}
+</script>
