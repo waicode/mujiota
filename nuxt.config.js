f40383b0de601b13af00f617bae1bdff1aa0dd7f
waicode <waicode37@gmail.com>
2021-10-31 20:59:58 +0900

    fix: dotenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,5 @@
 export default {
   publicRuntimeConfig: {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseUrl: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
     baseUrl: process.env.BASE_URL,
   },
   privateRuntimeConfig: {},
