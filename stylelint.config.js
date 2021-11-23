fe49e95888677fb07acaa4a38ad63967e9f47ad9
waicode <waicode37@gmail.com>
2021-11-23 19:44:41 +0900

    refactor: stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['stylelint-config-prettier', 'stylelint-config-recommended-scss'],
+  plugin: ['stylelint-scss'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-recommended-scss',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
